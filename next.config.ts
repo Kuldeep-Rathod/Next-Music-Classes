c86fe2b91f6ef7f7d3721ddb0da47e4204464869
Kuldeep Rathod <kuldiprathod375@gmail.com>
2025-03-29 14:53:55 +0530

    created navbar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "assets.aceternity.com",
+                port: "",
+                search: "",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
